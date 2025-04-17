4ac193c69c39cf0c96636c7a168fe4ca384f150d
Eugene Boondock <philosncube@gmail.com>
2025-04-17 15:48:31 +0200

    PWA

--- components/ServiceWorkerRegister.tsx
+++ components/ServiceWorkerRegister.tsx
@@ -3,7 +3,7 @@ import { useEffect } from "react";
 export default function ServiceWorkerRegister() {
   useEffect(() => {
     if ("serviceWorker" in navigator) {
-      navigator.serviceWorker.register("public/sw.js");
+      navigator.serviceWorker.register("/sw.js");
     }
   }, []);
   return null;
