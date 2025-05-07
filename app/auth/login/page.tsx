6130659b84d6a441d7c347b5ea77a762a47a61cf
Eugene Boondock <philosncube@gmail.com>
2025-05-07 16:52:30 +0200

    fixes

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,10 +1,14 @@
+import { Suspense } from "react";
+import { useSearchParams } from "next/navigation";
 import LoginForm from '@/components/auth/LoginForm'
 
 export default function LoginPage() {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-transparent relative z-10">
       <div className="w-full max-w-md flex flex-col items-center justify-center">
-        <LoginForm />
+        <Suspense fallback={<div>Loading...</div>}>
+          <LoginForm />
+        </Suspense>
       </div>
     </div>
   )
